c529dcf0e9b85e8dd63463dc230eba17c0c1d9fe
nyradhr <alessio.piccione@hotmail.it>
2022-07-25 15:23:52 +0200

    created models

--- src/main/java/it/accenture/footballclub/model/Game.java
+++ src/main/java/it/accenture/footballclub/model/Game.java
@@ -1,4 +1,40 @@
 package it.accenture.footballclub.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "GAME")
 public class Game {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "OPPOSING_TEAM")
+    private String opposingTeam;
+    @Column(name = "STADIUM")
+    private String stadium;
+    @Column(name = "DATE")
+    private LocalDate date;
+    @Column(name = "GALLERY_TICKETS")
+    private int galleryTickets;
+    @Column(name = "STAND_TICKETS")
+    private int standTickets;
+    @Column(name = "SOLD_TICKETS")
+    private int soldTickets;
+    @Column(name = "GALLERY_COST")
+    private int galleryCost;
+    @Column(name = "STAND_COST")
+    private int standCost;
+    @Column(name = "CANCELED")
+    private boolean canceled;
 }
--- src/main/java/it/accenture/footballclub/model/Membership.java
+++ src/main/java/it/accenture/footballclub/model/Membership.java
@@ -1,4 +1,32 @@
 package it.accenture.footballclub.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "MEMBERSHIP")
 public class Membership {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "START_DATE")
+    private LocalDate startDate;
+    @Column(name = "END_DATE")
+    private LocalDate endDate;
+    @Column(name = "COST")
+    private int cost;
+    @Column(name = "BUYER")
+    private String buyer;
+    @Column(name = "RECIPIENT")
+    private String recipient;
 }
