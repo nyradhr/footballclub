da94e9070eb67de12bb848a5d841c8407696b190
nyradhr <alessio.piccione@hotmail.it>
2022-07-25 14:40:26 +0200

    First commit: empty project save for model classes (empty)

---
+++ src/main/java/it/accenture/footballclub/FootballclubApplication.java
@@ -0,0 +1,13 @@
+package it.accenture.footballclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballclubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/it/accenture/footballclub/model/Game.java
@@ -0,0 +1,4 @@
+package it.accenture.footballclub.model;
+
+public class Game {
+}
